c71ccebb10e7d8ad084872fdc8ab592d3b0a57e1
Gabriele Pannuto <gabbi97@live.it>
2024-11-23 21:33:04 +0100

    Branch_Dev.Aeroporto scritto tutti i metodi

--- src/Spazi/Aeroporto.java
+++ src/Spazi/Aeroporto.java
@@ -1,6 +1,53 @@
 package Spazi;
 
+import Interfacce.CodeChecker;
+import Velivoli.*;
+
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Aeroporto {
+    private CodeChecker checker;
+    Set<Aeromobile> aerei= new TreeSet<>();
+
+    public Aeroporto(CodeChecker checker){
+        this.checker = checker;
+    }
+
+    public boolean inserisci(Aeromobile a){
+        return false;
+    }
+
+    public Aeromobile cerca(String codice){
+
+    }
+
+    public Aeromobile rimuovi(String codice){
+
+    }
+
+    public int elementi(){
+
+    }
+
+    public void salvaDOS(String nomefile){
+
+    }
+
+    public static Aeroporto leggiDIS(String nomefile){
+
+    }
+
+    public void salvaOBJ(String nomefile){
+
+    }
+
+    public static Aeroporto leggiOBJ(String nomefile){
+
+    }
+
+
+
 
 
 }
