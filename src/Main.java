ccdb6070f0eb8fb2790fe27a0a2777f26dc9d667
Gabriele Pannuto <gabbi97@live.it>
2024-12-03 13:46:19 +0100

    Salva obj da terminare e finire anche gli altri metodi

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 import CheckSuiCodici.CodeCheckIT;
 import Interfacce.CodeChecker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        CodeCheckIT ch;
-//        ch = new CodeCheckIT("ITMn988");
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Hell0 "+name);
 
 
     }
--- src/Spazi/Aeroporto.java
+++ src/Spazi/Aeroporto.java
@@ -1,50 +1,79 @@
 package Spazi;
 
 import Interfacce.CodeChecker;
-import Velivoli.*;
+import Velivoli.Aeromobile;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Aeroporto {
     private CodeChecker checker;
-    Set<Aeromobile> aerei= new TreeSet<>();
+    Set<Aeromobile> aerei = new HashSet<>();
 
     public Aeroporto(CodeChecker checker){
         this.checker = checker;
     }
 
     public boolean inserisci(Aeromobile a){
+        aerei.add(a);
+        if(aerei.contains(a))   return true;
         return false;
     }
 
     public Aeromobile cerca(String codice){
-
+        for(Aeromobile app : aerei){
+            if(app.getCodice().equals(codice))  return app;
+        }
+        return null;
     }
 
     public Aeromobile rimuovi(String codice){
-
+        Aeromobile app;
+        for(Aeromobile aereo : aerei){
+            if(aereo.getCodice().equals(codice)){
+                app = aereo;
+                aerei.remove(aereo);
+                return app;
+            }
+        }
+        return null;
     }
 
     public int elementi(){
+        int i=1;
+        for(Aeromobile app : aerei){
+            i++;
+        }
 
+        return i;
     }
 
     public void salvaDOS(String nomefile){
 
     }
 
-    public static Aeroporto leggiDIS(String nomefile){
-
-    }
+//    public static Aeroporto leggiDIS(String nomefile){
+//
+//    }
 
     public void salvaOBJ(String nomefile){
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(Double.parseDouble(System.out);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Non possibile Creare il file quì.");
+        } finally {
+            scanner.close();
+        }
 
     }
 
-    public static Aeroporto leggiOBJ(String nomefile){
-
-    }
+//    public static Aeroporto leggiOBJ(String nomefile){
+//
+//    }
 
 
 
